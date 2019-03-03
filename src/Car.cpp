0b84c273c725433c2f0ad4ce655d7f69be0a2824
monnier <monnier.jacques.lgm@gmail.com>
2019-03-03 14:56:50 +0100

    Contains the mother class
    Add the class method accelerate and display

--- src/Car.cpp
+++ src/Car.cpp
@@ -15,3 +15,14 @@ Car::Car(const Car &v){
 	this->speed 	= 	v.speed;
 	this->isStarted =	v.isStarted;
 }
+void Car::start(void){
+	setIsStarted(true);
+}
+
+void Car::accelerate (int v){}
+
+std::string Car::display(){
+	return std::to_string(getPower()) + "\n" + getColor() + "\n" +std::to_string(getSpeed()) + std::to_string(getIsStarted());
+}
+
+
